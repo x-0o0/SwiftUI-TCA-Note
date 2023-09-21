b5310bebf5149cb6427d57281ed89f7f07b631c1
Jaesung Lee <jaesung.lee@sendbird.com>
2023-09-21 19:10:28 +0900

    [수정] Theme 원래대로 수정
    - pointfree.co 의 Theme 은 Assets 기반이라서 코드 베이스로 변경

--- v1/Standups/Standups/Model/Theme.swift
+++ v1/Standups/Standups/Model/Theme.swift
@@ -36,7 +36,27 @@ enum Theme: String, CaseIterable, Equatable, Hashable, Identifiable, Codable {
         }
     }
     
-    var mainColor: Color { Color(self.rawValue) }
+    var mainColor: Color {
+        switch self {
+        case .bubblegum: return Color.purple
+        case .buttercup: return Color.brown
+        case .indigo: return Color.indigo
+        case .lavender: return Color.pink
+        case .magenta: return Color.red
+        case .navy: return Color.blue
+        case .orange: return Color.orange
+        case .oxblood: return Color.black
+        case .periwinkle: return Color.pink
+        case .poppy: return Color.mint
+        case .purple: return Color.purple
+        case .seafoam: return Color.mint
+        case .sky: return Color.cyan
+        case .tan: return Color.gray
+        case .teal: return Color.gray
+        case .yellow: return Color.yellow
+        }
+    }
+
     
     var name: String { self.rawValue.capitalized }
 }
